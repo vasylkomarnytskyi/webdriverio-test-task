270bb909ecd476ada7ed446f740c5d85f87ceef4
Vasyl Komarnytskyi <vk.vasylkomarnytskyi@gmail.com>
2025-08-01 14:22:12 +0300

    chore: update Prettier and ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,
+
+        $: 'readonly',
+        $$: 'readonly',
+        browser: 'readonly',
+        driver: 'readonly',
+        expect: 'readonly',
+      },
+    },
+  },
 ]);
